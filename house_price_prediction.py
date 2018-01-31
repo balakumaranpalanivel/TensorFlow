367b846fb597b9427153616143bc97a0b098f268
Balakumaran Palanivel <kumaraxe@gmail.com>
2018-01-31 21:15:29 +0000

    [MASTER] Trained the test data to a model

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -23,3 +23,82 @@ plt.plot(house_size, house_price, "bx") # bx = blue x
 plt.ylabel("Price")
 plt.xlabel("Size")
 plt.show()
+
+# prepare the data
+def normalize(array):
+    return (array - array.mean())/array.std()
+
+# train test split
+num_train_samples = math.floor(num_house * 0.7)
+
+# define training data
+train_house_size = np.asarray(house_size[:num_train_samples])
+train_price = np.asanyarray(house_price[:num_train_samples:])
+
+train_house_size_norm = normalize(train_house_size)
+train_price_norm = normalize(train_price)
+
+# define test data
+test_house_size = np.array(house_size[num_train_samples:])
+test_house_price = np.array(house_price[num_train_samples:])
+
+test_house_size_norm = normalize(test_house_size)
+test_house_price_norm = normalize(test_house_price)
+
+# putting data in tensor containers
+tf_house_size = tf.placeholder("float", name="house_size")
+tf_price = tf.placeholder("float", name="price")
+
+# tensor variables
+tf_size_factor = tf.Variable(np.random.randn(), name="size_factor")
+tf_price_offset = tf.Variable(np.random.randn(), name="price_offset")
+
+# inference operation
+tf_price_pred = tf.add(tf.multiply(tf_size_factor, tf_house_size),
+     tf_price_offset)
+
+# loss function
+tf_cost = tf.reduce_sum(tf.pow(tf_price_pred - tf_price, 2))/(2*num_train_samples)
+
+learning_rate = 0.1
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(tf_cost)
+
+init = tf.global_variables_initializer()
+
+# launch graph in the session
+with tf.Session() as sess:
+    sess.run(init)
+
+    # set how often to display training progress and iterations
+    display_every = 2
+    num_training_iter = 50
+
+    # keep iterating the data
+    for iteration in range(num_training_iter):
+
+        # fit all training data
+        for (x,y) in zip(train_house_size_norm, train_price_norm):
+            sess.run(optimizer, feed_dict={tf_house_size: x,
+                                            tf_price: y})
+
+        # display current status
+        if(iteration + 1) % display_every == 0:
+            c = sess.run(tf_cost, 
+            feed_dict={tf_house_size:train_house_size_norm,
+                        tf_price:train_price_norm})
+            print("iteration #:", '%04d' % (iteration+1),
+                "cost=", "{:.9f}".format(c),
+                "size_factor=", sess.run(tf_size_factor),
+                "price_offset=", sess.run(tf_price_offset))
+
+    print("Optimisation Finished")
+    training_cost = sess.run(tf_cost, 
+            feed_dict={tf_house_size: train_house_size_norm,
+                    tf_price:train_price_norm})
+    print("Trained cost=", training_cost,
+     "size_factor=", sess.run(tf_size_factor),
+     "price_offset=", sess.run(tf_price_offset), '\n')
+
+
+     
