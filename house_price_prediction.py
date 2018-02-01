de5502618d4e477adc90ce116d647771f66dea93
Balakumaran Palanivel <kumaraxe@gmail.com>
2018-02-01 06:11:29 +0000

    [MASTER] Learning regression completed. Line predicted plot is wrong

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -100,5 +100,25 @@ with tf.Session() as sess:
      "size_factor=", sess.run(tf_size_factor),
      "price_offset=", sess.run(tf_price_offset), '\n')
 
+    train_house_size_mean = train_house_size.mean()
+    train_house_size_std = train_house_size.std()
+
+    train_price_mean = train_price.mean()
+    train_price_std = train_price.std()
+
+     # plot the graph
+    plt.rcParams["figure.figsize"] = (10, 8)
+    plt.figure()
+    plt.ylabel("Price")
+    plt.xlabel("Size (sq.ft)")
+    plt.plot(train_house_size, train_price, 'go', label='Training Data')
+    plt.plot(test_house_size, test_house_price, 'mo', label='Testing Data')
+    plt.plot(train_house_size_norm * train_house_size_std + train_house_size_mean,
+     (sess.run(tf_size_factor) * train_house_size_norm + sess.run(tf_price_offset)) * train_house_size_std + train_price_mean,
+     label = 'Learned Regression')
+
+    plt.legend(loc="upper left")
+    plt.show()
+
 
      
